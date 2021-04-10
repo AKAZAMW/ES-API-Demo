9ead5f34656a87539c91375de3f2ae4cb74b4ff2
robot <zxxia@akazam.com>
2021-04-10 12:06:29 +0800

    demo

--- src/components/es-tv-demos/demo.vue
+++ src/components/es-tv-demos/demo.vue
@@ -1,9 +1,12 @@
 <template>
   <div class="container2">
-    <div>
-    <img class="guide"  src="http://120.236.119.11:58011/ivod/jiangsuVR/img/home/help2.jpg"
-         focusView :focusable="true" :requestFocus="true" @click="clickView"/>
-    <img class="feedback" src="http://120.236.119.11:58011/ivod/jiangsuVR/img/home/feedback_selected.png"/>
+    <div v-if="showGuide">
+      <img class="guide"  src="http://120.236.119.11:58011/ivod/jiangsuVR/img/home/help2.jpg"/>
+      <img class="feedback" :focusable="true" :requestFocus="true" @click="clickView"
+           src="http://120.236.119.11:58011/ivod/jiangsuVR/img/home/feedback_selected.png"/>
+    </div>
+    <div v-if="!showGuide" class="report">
+      <p class="title">故障上报</p>
     </div>
   </div>
 </template>
@@ -13,7 +16,13 @@
   import '/static/css/common.css'
 
   export default {
+    data() {
+      return {
+        showGuide: true,
+      };
+    },
     mounted() {
+      this.showGuide = false;
       this.app = getApp();
       this.app.$on('hardwareBackPress', this.backPress);
     },
@@ -23,13 +32,25 @@
       },
       clickView() {
         console.log('hello');
+        this.showGuide = !this.showGuide;
       },
     }
   }
 </script>
 
-<style scope>
+<style>
+.container2 .report{
 
+}
+.container2 .report .title{
+  line-height: 70px;
+  text-align: center;
+  font-size: 36px;
+  font-weight: 400;
+  margin-top: 60px;
+  font-family: 'Microsoft YaHei UI';
+  color: #FFFFFF;
 
+}
 
 </style>
